8f1d743943a0bf98f72a71c226700e770577e331
Ranjita <ranjitarai350@gmail.com>
2024-09-18 10:34:10 +0545

    1st commit

---
+++ practice/arithOpr.py
@@ -0,0 +1,11 @@
+'''num1=30
+num2=40
+Sum=num1+num2
+print(Sum)'''
+
+def sum_opr():
+    num1=int(input("Enter First Num: "))
+    num2=int(input("Enter Second Num: "))
+    sum=num1+num2
+    print(sum)
+sum_opr()
---
+++ practice/bitOR.py
@@ -0,0 +1,13 @@
+'''num1=5
+num2=3
+print(num1|num2)
+'''
+def Bit_OR():
+    num1=int(input("Enter 1st number: "))
+    num2=int(input("Enter 2nd number: "))
+    result=num1 | num2
+    print(result)
+
+Bit_OR()
+
+    
