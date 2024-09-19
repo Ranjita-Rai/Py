a6ef9e9f40dbb0c3b127a08944b3663520d4e96d
Ranjita <ranjitarai350@gmail.com>
2024-09-19 09:59:39 +0545

    2nd commit

---
+++ DiffFunctions/OddEven.py
@@ -0,0 +1,16 @@
+'''num=10
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+'''
+
+
+def Odd_Even():
+    num=int(input("Enter a Number: "))
+    if num%2==0:
+        print("Number is Even")
+    else:
+        print("Number is Odd")
+
+Odd_Even()
---
+++ DiffFunctions/StarPrint.py
@@ -0,0 +1,7 @@
+def Star_Print(num):
+    for i in range (1, num+1):
+       # print(i) #just for undertanding how actually works 
+        print("*" * i)
+
+n=int(input("Enter a number: "))
+Star_Print(n)
