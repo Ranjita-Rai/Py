2afbdafb6073ecb5f84ba47e6a445e6ffd9f219b
Ranjita <ranjitarai350@gmail.com>
2024-09-22 13:13:36 +0545

    add another files

---
+++ Operators/AssignmentOpr.py
@@ -0,0 +1,6 @@
+def Assignment_opr():
+    num=int(input("Enter 1st NUmber: "))
+    num+=num
+    print(num)
+
+Assignment_opr()
---
+++ Operators/logicalAnd.py
@@ -0,0 +1,8 @@
+def Logical_and():
+    num1=int(input("Enter 1st num: "))
+    num2=int(input("Enter 2nd num: "))
+    result=num1 and num2
+    print(result)
+
+Logical_and()
+
