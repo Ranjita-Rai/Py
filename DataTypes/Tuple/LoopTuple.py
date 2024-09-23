f6a9e147f23995cbd4c98b5c42d709f56284c6ad
Ranjita <ranjitarai350@gmail.com>
2024-09-23 12:53:24 +0545

    add LoopTuple file

---
+++ DataTypes/Tuple/LoopTuple.py
@@ -0,0 +1,13 @@
+newTuple=("Apple","Banana","Orange")
+#for loop
+for i in newTuple:
+    print(i)
+
+for i in range(len(newTuple)):
+    print(newTuple[i])
+
+#while loop
+i=0
+while i<len(newTuple):
+    print(newTuple[i])
+    i=i+1;
