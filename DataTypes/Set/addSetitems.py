f8c35cfa080054d9c73249ddae22cc3725a08881
Ranjita <ranjitarai350@gmail.com>
2024-09-29 13:11:19 +0545

    add 2 files

---
+++ DataTypes/Set/addSetitems.py
@@ -0,0 +1,14 @@
+fruits={"Apple", "Banana", "Orange"}
+
+#using add() method
+fruits.add("Mango") 
+print(fruits)
+
+#using update() method
+newfruits={"Cherry", "Blueberry","Guava"}
+fruits.update(newfruits)
+print(fruits)
+
+newlist=["Avocado","Strawberry"]
+fruits.update(newlist)
+print(fruits)
---
+++ DataTypes/Set/removeItems.py
@@ -0,0 +1,21 @@
+name={"Ranjita", "Sanjita","Ranjana","Anjana"}
+#using discard() method
+name.discard("Ranjana")
+print(name)
+
+#using remove() method
+name.remove("Sanjita")
+print(name)
+
+
+
+'''
+#using clear()  #empty set
+name.clear()
+print(name)
+'''
+
+
+ #using del keyword    #delete entire set
+del name 
+print(name)
