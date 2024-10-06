c97ba416933e7299b16add0e7b1a095839507ae9
Ranjita <ranjitarai350@gmail.com>
2024-10-06 08:12:40 +0545

    add class file

--- OOP/class.py
+++ OOP/class.py
@@ -11,15 +11,28 @@ print(newclass.a) #print value of a
 obj=newclass()
 print(obj.b) 
 
-#using __init__()
+#using __init__() function
 # #All classes have a function called __init__(), which is always executed when the class is being initiated.
 #Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
 class Person:
-  def __init__(person, name, address):
-    person.name = name
-    person.address = address
+  def __init__(self, name, address):
+    self.name = name
+    self.address = address
 
 obj = Person("Sanjana", "Ktm")
 
 print(obj.name)
 print(obj.address)
+
+#Using __str__()function
+#The __str__() function is used to return a string representation of the object.
+class student:
+   def __init__(self,id,name,address,age):
+      self.id=id
+      self.name=name
+      self.address=address
+      self.age=age
+   def __str__(self):
+      return f"{self.id} {self.name} {self.address} {self.age}"
+obj=student(1,"Sanjana","Ktm",20)
+print(obj)
