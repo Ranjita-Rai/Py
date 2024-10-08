fdc00f7fe407a9631ab075e1aa230b6e909326b9
Ranjita <ranjitarai350@gmail.com>
2024-10-08 10:36:56 +0545

    Modified inheritence file

--- OOP/Inheritence.py
+++ OOP/Inheritence.py
@@ -2,15 +2,40 @@
 #child class  is inheriting the properties of the parent class
 
 class parent:
-    def __init__(self, name, address):
-        self.name=name
+    def __init__(self, name, address):  # constructor
+        self.name=name 
         self.address=address
 
-    def print(self):
-        print(self.name, self.address)     
+    def print(self): 
+        print(self.name, self.address) 
 
-class child(parent):
-    pass
+class child(parent):   # child class is inheriting the properties of the parent class
+    pass   #pass keyword is used to define a function or class without any code
 
-obj=parent("Ranjita", "Nuwakot")
-obj.print()
+obj=parent("Ranjita", "Nuwakot")   # creating an object of the parent class
+obj.print()   # calling the method of the parent class
+
+
+
+####QQ###
+class car:
+    color="black"
+    @staticmethod
+    def start(self):
+        print("car started..")
+    
+    @staticmethod
+    def stop(self):
+        print("car stopped..")
+
+class Toyota(car):
+    def __init__(self, name):
+        self.name=name
+
+car1=Toyota("Fortuner")
+car2=Toyota("pirus")
+print(car1.start)
+print(car1.name)
+print(car2.name)
+print(car1.color)
+print(car2.color)
